7d38272f04eba2dd25a08bfd4cf3f1c384ae83d2
Todd W Crone <twcrone@gmail.com>
2023-11-28 21:53:46 -0500

    Looke for weak words

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::mem::size_of_val;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -34,6 +38,39 @@ fn main() {
     vec.sort_by(|a, b| b.1.cmp(&a.1));
 
     for (word, count) in & vec {
-        println!("{}={}", word, count);
+    //    println!("{}={}", word, count);
     }
+    let passive_words = get_passive();
+    for word in & passive_words {
+        let count = count_occurrences(word, &contents);
+        if count > 0 {
+            println!("{}={}", word, count);
+        }
+    }
+    //println!("{:?}", passive_words);
 }
+
+fn get_passive() -> Vec<String> {
+    let mut passive_words: Vec<String> = Vec::new();
+    if let Ok(lines) = read_lines("passive.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(word) = line {
+                passive_words.push(word)
+            }
+        }
+    }
+    passive_words
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn count_occurrences(pattern: &String, body: &String) -> usize {
+    let v = body.matches(pattern);
+    v.count()
+}
+
